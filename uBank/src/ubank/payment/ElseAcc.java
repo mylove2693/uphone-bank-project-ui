a268a2cf3012b60e43ebd88e42baa78710727bd1
gsm10228 <gsm10228@1c57499f-2e70-8b8f-4783-ec56c64a0f57>
2011-04-09 10:07:21 +0000

    主配置文件都加了 <!-- 			<intent-filter> 				<action android:name="android.intent.action.MAIN"/> 				<category android:name="android.intent.category.LAUNCHER" /> 			</intent-filter> --> check下来将punnan的注释掉,去掉自己的注释

---
+++ uBank/src/ubank/payment/ElseAcc.java
@@ -0,0 +1,52 @@
+package ubank.payment;
+
+import ubank.base.GeneralActivity;
+import ubank.main.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ElseAcc extends GeneralActivity {
+
+	private TextView txt=null;
+	private Button next_btn=null;
+	@Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        addLayout(R.layout.account_type);
+	        
+	        tvClassFirst.setVisibility(View.VISIBLE);
+	        tvClassFirst.setText("首页>");
+	        tvClassSecond.setVisibility(View.VISIBLE);
+	        tvClassSecond.setText("自助缴费>");
+	       //监听
+	        setListener(tvClassSecond, this, AllPaymentSer.class);
+	        tvClassThird.setVisibility(View.VISIBLE);
+	        tvClassThird.setText("其他账户选择");
+	        
+	        //确认缴费按钮
+	        next_btn=(Button)findViewById(R.id.account_type_comfirm).findViewById(R.id.button);
+	        next_btn.setText("下一步");
+	        next_btn.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent=new Intent();
+					/**
+					 * 将服务器上取得的值传给下一个Activity
+					 */
+					String account="222222";
+					String money="20";
+					intent.putExtra("account", account);
+					intent.putExtra("money", money);
+					intent.setClass(ElseAcc.this, InputPsw.class);
+					ElseAcc.this.startActivity(intent);
+				}
+			});
+	  }
+}
+
+
