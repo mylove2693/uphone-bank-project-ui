b41ae7194ce976253d8967357242e4e120557826
wangting0919@gmail.com <wangting0919@gmail.com@1c57499f-2e70-8b8f-4783-ec56c64a0f57>
2011-04-14 08:20:38 +0000

    账户信息及余额查询   连接到后台

--- uBank/src/ubank/account_query/AccountQueryInfo.java
+++ uBank/src/ubank/account_query/AccountQueryInfo.java
@@ -1,12 +1,19 @@
 package ubank.account_query;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ubank.base.GeneralListActivity;
+import ubank.enum_type.EAccType;
+import ubank.enum_type.EOperation;
 import ubank.main.BankMain;
+import ubank.webservice.ConnectWs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class AccountQueryInfo extends GeneralListActivity {
+	private Intent intent = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -20,14 +27,54 @@ public class AccountQueryInfo extends GeneralListActivity {
 		setListener(tvClassSecond, this, AccountQueryType.class);
 		tvClassThird.setVisibility(View.VISIBLE);
 		tvClassThird.setText("账户信息及余额查询");
+		intent = this.getIntent();
 		
-		Intent intent = this.getIntent();
-		String[] name = new String[]{"账户","账户类型","币种","余额","存期","起息月","利率"};
-		String[] value = new String[]{intent.getStringExtra("accNumValue"),
-									  intent.getStringExtra("accTypeValue"),
-									  "人民币","100000","三个月","2010-11",
-									  "1.5%"};
+		setAccInfo();
+	}
+
+	private void setAccInfo() {
+		String[] name;
+		String[] value;
+		JSONObject json = null;
+		String accType = intent.getStringExtra("accTypeValue");
+		if ("信用卡".equals(accType)) {
+			name = new String[]{"账户","账户类型","币种","信用额度","可用度","预借现金可用度","每月的账单日","本期还款额","最低还款额","到期还款日"};
+			value = new String[name.length];
+			json = ConnectWs.connect(this, EAccType.CREDIT_CARD, EOperation.GET_ACC_INFO,
+					intent.getStringExtra("accNumValue"));
+			try {
+				for (int i = 0; i < name.length; i++) {
+					value[i] = json.getString(name[i]);
+				}
+			} catch (JSONException ex) {
+				ex.printStackTrace();
+			}
+		} else if ("定期储蓄卡".equals(accType)) {
+			name = new String[]{"账户","账户类型","币种","余额","存期","起息月","利率"};
+			value = new String[name.length];
+			json = ConnectWs.connect(this, EAccType.TIME_DEPOSITS, EOperation.GET_ACC_INFO,
+					intent.getStringExtra("accNumValue"));
+			try {
+				for (int i = 0; i < name.length; i++) {
+					value[i] = json.getString(name[i]);
+				}
+			} catch (JSONException ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			name = new String[]{"账户","账户类型","币种","余额"};
+			value = new String[name.length];
+			json = ConnectWs.connect(this, EAccType.CURRENT_DEPOSIT, EOperation.GET_ACC_INFO,
+					intent.getStringExtra("accNumValue"));
+			try {
+				for (int i = 0; i < name.length; i++) {
+					value[i] = json.getString(name[i]);
+				}
+			} catch (JSONException ex) {
+				ex.printStackTrace();
+			}
+		}
 		
-		setListAdapter(createText_Text(name,value));
+		setListAdapter(createText_Text(name, value));
 	}
 }
